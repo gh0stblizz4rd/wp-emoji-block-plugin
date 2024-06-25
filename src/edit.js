37f0e9cc8c15b5f5694901277bde1e8ac41cfb8e
Romans Audzeris <romansa772@aol.com>
2024-06-25 17:16:15 +0300

    Emoji picker window added

--- src/edit.js
+++ src/edit.js
@@ -1,14 +1,22 @@
 import { useBlockProps } from "@wordpress/block-editor";
+import { useState } from "react";
 import EmojiPicker from "emoji-picker-react";
 import "./editor.scss";
 
 export default function Edit() {
+	const [emojiPickerActive, setEmojiPickerActive] = useState(false);
+
 	return (
 		<div {...useBlockProps()}>
 			<div className="emoji-selector">
 				<div className="selected-emoji">😊</div>
 				<div className="emoji-edit-buttons-wrapper">
-					<button className="choose-emoji emoji-edit-buttons">
+					<button
+						className="choose-emoji emoji-edit-buttons"
+						onClick={() => {
+							setEmojiPickerActive(true);
+						}}
+					>
 						Choose emoji
 					</button>
 					<button className="remove-emoji emoji-edit-buttons">
@@ -16,7 +24,11 @@ export default function Edit() {
 					</button>
 				</div>
 			</div>
-			{/* <EmojiPicker /> */}
+			{emojiPickerActive ? (
+				<EmojiPicker className="emoji-picker-window" />
+			) : (
+				"not active"
+			)}
 		</div>
 	);
 }
