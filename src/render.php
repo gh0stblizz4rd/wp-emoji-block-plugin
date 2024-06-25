9f6518182b34c3677cab7045e716db2e109f335a
Romans Audzeris <romansa772@aol.com>
2024-06-25 16:46:39 +0300

    Emoji can be saved to atributes and displayed on the frontend

--- src/render.php
+++ src/render.php
@@ -3,6 +3,7 @@
  * @see https://github.com/WordPress/gutenberg/blob/trunk/docs/reference-guides/block-api/block-metadata.md#render
  */
 ?>
-<p <?php echo get_block_wrapper_attributes(); ?>>
-	<?php esc_html_e( 'Emoji Block – hello from a dynamic block!', 'emoji-block' ); ?>
-</p>
+<div class="emoji-block-wrapper">
+	<pre class="emoji-block-data" style="display: none"><?php echo wp_json_encode($attributes) ?></pre>
+	<div class="emoji-block-root"></div>
+</div>
